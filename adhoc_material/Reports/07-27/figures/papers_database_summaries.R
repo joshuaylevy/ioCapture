89e4187f6ec42fe488aa2cb608f1254461c850e9
Joshua Levy <joshua.levy@chicagobooth.edu>
2022-07-26 14:36:44 -0500

    Initial Scopus-econlit match figures/tables

--- adhoc_material/Reports/07-27/figures/papers_database_summaries.R
+++ adhoc_material/Reports/07-27/figures/papers_database_summaries.R
@@ -1,10 +1,72 @@
 library(tidyverse)
-
+library(stargazer)
 
 print(getwd())
-setwd('adhoc_material/Reports/07-27/figures')
+setwd("adhoc_material/Reports/07-27/figures")
+path_prepend_00_scraping <- "../../../../00_Scraping/"
+path_prepend_01_database <- "../../../../01_Database_Construction/"
+
+
+fuzzy_reports_read <- read_csv(paste0(path_prepend_01_database, "papers/fuzzy_match_results.csv", sep = ""))
+fuzzy_reports_df <- fuzzy_reports_read %>%
+    select(-c(`...1`)) %>%
+    pivot_longer(cols = -c(pub_code), names_to = "indicator", values_to = "value") %>%
+    filter(indicator %in% c(
+        "naive_match_obs",
+        "fuzzy_match_obs",
+        "remaining_econlit_obs",
+        "remaining_scopus_obs"
+    )) %>%
+    mutate(
+        pub_type = case_when(
+            pub_code %in% c("AER", "ECA", "JPE", "QJE", "RES") ~ "Top 5",
+            pub_code %in% c("ATB", "JEM", "JHR", "JLE", "JLO", "RJE") ~ "IO & Law",
+            pub_code %in% c("JFE", "JOF", "RFS") ~ "Finance",
+            pub_code %in% c("JOL") ~ "Labor"
+        ),
+        pub_code = as.factor(pub_code),
+        pub_code = fct_relevel(pub_code, "AER", "ECA", "JPE", "QJE", "RES", "ATB", "JEM", "JHR", "JLE", "JLO", "RJE", "JFE", "JOF", "RFS", "JOL")
+    ) %>%
+    view()
 
 
 
-fuzzy_reports_df <- read_csv('../../../../01_Database_Construction/papers/fuzzy_match_results.csv')
+
+ggplot(fuzzy_reports_df) +
+    geom_col(aes(x = pub_code, y = value, fill = indicator, group = pub_code),
+        position = "stack",
+        color = "white",
+        width = 0.6
+    ) +
+    scale_fill_discrete(breaks = c("naive_match_obs", 
+            "fuzzy_match_obs",
+            "remaining_scopus_obs",
+            "remaining_econlit_obs"),
+        labels = c("Naively Matched",
+            "Fuzzily Matched", 
+            "Unmatched Scopus", 
+            "Unmatched EconLit"),
+        name = "Type:") +
+    ggtitle("Results of Scopus-EconLit Matching")+
+    xlab("")+
+    ylab("Observations (articles)")+
+    theme_minimal()+
+    theme(
+        plot.title = element_text(size = 20),
+        legend.background =  element_rect(fill = "white", color = NA),
+        legend.position = c(0.9, 0.8)
+    )
+
+stargazer(fuzzy_reports_read %>%
+    select(-c(`...1`, scopus_obs, econlit_obs)),
+    type = "latex",
+    title = "Results of Scopus-EconLit Matching",
+    style = "AER", 
+    summary = FALSE,
+    column.labels = c('Journal',
+        'Naive matches',
+        'Fuzzy matches',
+        'Scopus only',
+        'EconLit only')
+    )
 
