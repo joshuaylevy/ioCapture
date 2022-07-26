c809cdd879a3f20f7f7cb0eae79cd1b7a6e06669
Joshua Levy <joshua.levy@chicagobooth.edu>
2022-07-26 17:18:51 -0500

    Getting Tables moving
    tex_table_modifier.ipynb
    - Have an automated table-modifier in place now
    - So far it's set up to modify table 2 (generated by _____) adding an extra panels column, and formatting the flush-left text of the first two columns
    - This generates scopus_econlit_matching_results_out.tex
    
    papers_database_summaries.R
    - This is an R Script which manages all of the fuzzy_reports and concatenates the fuzzy results to 1) generate a papers database; and 2) output figures and tables describing the results of the fuzzy-matching process (already done)
    - This generates scopus_econlit_matching_results.png
    - This generates scopus_econlit_matching_results_in.tex
    
    
    misc

--- adhoc_material/Reports/07-27/figures/papers_database_summaries.R
+++ adhoc_material/Reports/07-27/figures/papers_database_summaries.R
@@ -38,35 +38,50 @@ ggplot(fuzzy_reports_df) +
         color = "white",
         width = 0.6
     ) +
-    scale_fill_discrete(breaks = c("naive_match_obs", 
+    scale_fill_discrete(
+        breaks = c(
+            "naive_match_obs",
             "fuzzy_match_obs",
             "remaining_scopus_obs",
-            "remaining_econlit_obs"),
-        labels = c("Naively Matched",
-            "Fuzzily Matched", 
-            "Unmatched Scopus", 
-            "Unmatched EconLit"),
-        name = "Type:") +
-    ggtitle("Results of Scopus-EconLit Matching")+
-    xlab("")+
-    ylab("Observations (articles)")+
-    theme_minimal()+
+            "remaining_econlit_obs"
+        ),
+        labels = c(
+            "Naively Matched",
+            "Fuzzily Matched",
+            "Unmatched Scopus",
+            "Unmatched EconLit"
+        ),
+        name = "Type:"
+    ) +
+    ggtitle("Results of Scopus-EconLit Matching") +
+    xlab("") +
+    ylab("Observations (articles)") +
+    theme_minimal() +
     theme(
         plot.title = element_text(size = 20),
-        legend.background =  element_rect(fill = "white", color = NA),
+        legend.background = element_rect(fill = "white", color = NA),
         legend.position = c(0.9, 0.8)
     )
+ggsave(filename = "../figures/scopus_econlit_matching_results.png", plot = last_plot(), width = 297, height = 210, units = "mm")
+
 
 stargazer(fuzzy_reports_read %>%
-    select(-c(`...1`, scopus_obs, econlit_obs)),
+    select(-c(`...1`, scopus_obs, econlit_obs)) %>%
+    mutate(
+        pub_code = as.factor(pub_code),
+        pub_code = fct_relevel(pub_code, "AER", "ECA", "JPE", "QJE", "RES", "ATB", "JEM", "JHR", "JLE", "JLO", "RJE", "JFE", "JOF", "RFS", "JOL")
+    )  %>%
+    arrange(pub_code) %>%
+    mutate(pub_code = as.character(pub_code)) %>%
+    rename('Journal' = pub_code,
+        'Naive matches' = naive_match_obs, 
+        'Fuzzy matches' = fuzzy_match_obs, 
+        'Scopus only' = remaining_scopus_obs, 
+        'EconLit only' = remaining_econlit_obs),
     type = "latex",
     title = "Results of Scopus-EconLit Matching",
-    style = "AER", 
+    style = "AER",
+    out = "../tables/input/scopus_econlit_matching_results_in.tex",
     summary = FALSE,
-    column.labels = c('Journal',
-        'Naive matches',
-        'Fuzzy matches',
-        'Scopus only',
-        'EconLit only')
-    )
-
+    rownames = FALSE,
+    out.header = FALSE)
