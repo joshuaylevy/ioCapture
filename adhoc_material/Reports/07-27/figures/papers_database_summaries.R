ab7dc64f7d74f38e74bf2a005cbb3efd199c9646
Joshua Levy <joshua.levy@chicagobooth.edu>
2022-07-26 13:22:36 -0500

    Preparing database collation
    post_fuzzy_papers_db_construction .ipuynb
    - This script has two primary purposes.
    - 1.It reads fuzzy-matching reports so that all of that information can be displayed in a single format.
    - 2. It starts concatenating the fuzzy_match results/hand-matched results (we have yet to implement the actual hand-matching pending the 07/28 meeting) to create a single papers database.

---
+++ adhoc_material/Reports/07-27/figures/papers_database_summaries.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+
+
+print(getwd())
+setwd('adhoc_material/Reports/07-27/figures')
+
+
+
+fuzzy_reports_df <- read_csv('../../../../01_Database_Construction/papers/fuzzy_match_results.csv')
+
